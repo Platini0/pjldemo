d6794969e562802ddb6c6a0afee198811bd154a0
Phil Looby <philip@silverfoxit.co.uk>
2013-03-16 07:21:04 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PjldemoApp::Application.config.secret_token = '83de806ad4f54750b565a5ee6774ef86a24e8bd8278ea30ac3fe99b7bc8eb294361b88a5b826e9b26a14cc3a8d4b36665b451d2debc54a28e0f4fec0c8f0d8ba'
